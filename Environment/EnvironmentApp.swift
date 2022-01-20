5e43875f17d0879b0ed4c017c41cebea814658ef
Code Palace <codepalace@fastmail.se>
2022-01-20 10:38:57 +0100

    Initial Commit

---
+++ Environment/EnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentApp.swift
+//  Environment
+//
+//  Created by Federico on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
